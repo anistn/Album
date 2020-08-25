5e0466fce02767e58d98363f3fb7cf41540282a0
Anis BEN NSIR <anis.bennsir@gmail.com>
2020-08-25 21:18:03 +0200

    add ui data

--- app/src/main/java/fr/abennsir/poc/album/app/gallery/data/Data.kt
+++ app/src/main/java/fr/abennsir/poc/album/app/gallery/data/Data.kt
@@ -2,6 +2,7 @@ package fr.abennsir.poc.album.app.gallery.data
 
 import android.os.Parcelable
 import androidx.annotation.Keep
+import fr.abennsir.poc.album.domain.data.Photo
 import kotlinx.android.parcel.Parcelize
 
 @Keep
@@ -13,4 +14,26 @@ data class Config(
 
 enum class NavigationMode {
     DEFAULT, SECTIONED
+}
+
+sealed class UiModel {
+    @Parcelize
+    data class PhotoItem(
+        val id: Long = 0, val albumId: Long = 0, val title: String = "",
+        val url: String = "", val thumbnailUrl: String = ""
+    ) : UiModel(),
+        Parcelable
+
+
+    data class AlbumItem(val title: String, val id: Long) : UiModel()
+}
+
+fun Photo.toUiData() = UiModel.PhotoItem(
+    id = id, albumId = albumId, thumbnailUrl = thumbnailUrl,
+    title = title, url = url
+)
+
+
+enum class ScreenState {
+    EMPTY, LOADING, REFRESH, IDLE, EMPTY_ERROR
 }
